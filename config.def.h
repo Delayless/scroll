5381491a6a64a87af3348e34fad5df1db1f81c1c
Delayless <Delayless@gmail.com>
2021-02-23 13:35:32 +0800

    perf(config.def.h): add keybinds

--- config.def.h
+++ config.def.h
@@ -11,6 +11,12 @@ struct rule rules[] = {
 	{"\033[5;2~",   SCROLL_UP,   -1},	/* [Shift] + [PageUP] */
 	{"\033[6;2~",   SCROLL_DOWN, -1},	/* [Shift] + [PageDown] */
 	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
-	//{"\031",        SCROLL_UP,    1},	/* mouse wheel up */
-	//{"\005",        SCROLL_DOWN,  1},	/* mouse wheel Down */
+	/* {"\031",        SCROLL_UP,    1},	/1* mouse wheel up *1/ */
+	/* {"\005",        SCROLL_DOWN,  1},	/1* mouse wheel Down *1/ */
+	{"k",         SCROLL_UP,    1},
+	{"j",         SCROLL_DOWN,  1},
+	{"K",         SCROLL_UP,   20},
+	{"J",         SCROLL_DOWN, 20},
+	{"u",         SCROLL_UP,   20},
+	{"d",         SCROLL_DOWN, 20},
 };
